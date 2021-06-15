78d7a7c699cb8f12a644258794eb9d71c37ac628
smoogipoo <smoogipoo@smgi.me>
2021-06-15 12:13:05 +0900

    Indent using spaces

--- setup.sh
+++ setup.sh
@@ -1,62 +1,62 @@
 #!/bin/bash
 
 if [[ -z "${OSU_SERVER_REPO}" ]]; then
-	OSU_SERVER_REPO=ppy/osu-server
+    OSU_SERVER_REPO=ppy/osu-server
 fi
 
 if [[ -z "${OSU_REPO}" ]]; then
-	OSU_REPO=ppy/osu
+    OSU_REPO=ppy/osu
 fi
 
 if [[ -z "${PP_REPO}" ]]; then
-	PP_REPO=ppy/osu-performance
+    PP_REPO=ppy/osu-performance
 fi
 
 if [[ -z "${WEB_REPO}" ]]; then
-	WEB_REPO=smoogipoo/osu-web
+    WEB_REPO=smoogipoo/osu-web
 fi
 
 if [[ -z "${ES_REPO}" ]]; then
-	ES_REPO=ppy/osu-elastic-indexer
+    ES_REPO=ppy/osu-elastic-indexer
 fi
 
 if [[ -z "${OSU_SERVER_BRANCH}" ]]; then
-	OSU_SERVER_BRANCH=master
+    OSU_SERVER_BRANCH=master
 fi
 
 if [[ -z "${OSU_BRANCH}" ]]; then
-	OSU_BRANCH=master
+    OSU_BRANCH=master
 fi
 
 if [[ -z "${PP_BRANCH}" ]]; then
-	PP_BRANCH=master
+    PP_BRANCH=master
 fi
 
 if [[ -z "${WEB_BRANCH}" ]]; then
-	WEB_BRANCH=pp-tester
+    WEB_BRANCH=pp-tester
 fi
 
 if [[ -z "${ES_BRANCH}" ]]; then
-	ES_BRANCH=master
+    ES_BRANCH=master
 fi
 
 if [[ -z "${MODE}" ]]; then
-	MODE=0
+    MODE=0
 fi
 
 case "${MODE}" in
-	0)
-		MODE_LITERAL=osu
-		;;
-	1)
-		MODE_LITERAL=taiko
-		;;
-	2)
-		MODE_LITERAL=catch
-		;;
-	3)
-		MODE_LITERAL=mania
-		;;
+    0)
+        MODE_LITERAL=osu
+        ;;
+    1)
+        MODE_LITERAL=taiko
+        ;;
+    2)
+        MODE_LITERAL=catch
+        ;;
+    3)
+        MODE_LITERAL=mania
+        ;;
 esac
 
 echo "
@@ -86,91 +86,91 @@ DATA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIR=$(pwd)
 
 # osu-web
-	echo "Cloning $WEB_REPO into $(pwd)..."
+    echo "Cloning $WEB_REPO into $(pwd)..."
 
-	git clone https://github.com/$WEB_REPO .
-	git checkout -f origin/$WEB_BRANCH
+    git clone https://github.com/$WEB_REPO .
+    git checkout -f origin/$WEB_BRANCH
 
 # osu-performance
-	mkdir -p $DIR/osu-performance
-	cd $DIR/osu-performance
+    mkdir -p $DIR/osu-performance
+    cd $DIR/osu-performance
 
-	echo "Cloning $PP_REPO into $(pwd)..."
+    echo "Cloning $PP_REPO into $(pwd)..."
 
-	git clone --recurse-submodules https://github.com/$PP_REPO .
-	git checkout -f origin/$PP_BRANCH
-	git submodule update --init --recursive
+    git clone --recurse-submodules https://github.com/$PP_REPO .
+    git checkout -f origin/$PP_BRANCH
+    git submodule update --init --recursive
 
 # osu
-	mkdir -p $DIR/osu
-	cd $DIR/osu
+    mkdir -p $DIR/osu
+    cd $DIR/osu
 
-	echo "Cloning $OSU_REPO into $(pwd)..."
+    echo "Cloning $OSU_REPO into $(pwd)..."
 
-	git clone --recurse-submodules https://github.com/$OSU_REPO .
-	git checkout -f origin/$OSU_BRANCH
+    git clone --recurse-submodules https://github.com/$OSU_REPO .
+    git checkout -f origin/$OSU_BRANCH
 
 # osu-server
-	mkdir -p $DIR/osu-server
-	cd $DIR/osu-server
+    mkdir -p $DIR/osu-server
+    cd $DIR/osu-server
 
-	echo "Cloning $OSU_SERVER_REPO into $(pwd)..."
+    echo "Cloning $OSU_SERVER_REPO into $(pwd)..."
 
-	git clone --recurse-submodules https://github.com/$OSU_SERVER_REPO .
-	git checkout -f origin/$OSU_SERVER_BRANCH
+    git clone --recurse-submodules https://github.com/$OSU_SERVER_REPO .
+    git checkout -f origin/$OSU_SERVER_BRANCH
 
     ./UseLocalOsu.sh
 
 # es
-	mkdir -p $DIR/osu-elastic-indexer
-	cd $DIR/osu-elastic-indexer
+    mkdir -p $DIR/osu-elastic-indexer
+    cd $DIR/osu-elastic-indexer
 
-	echo "Cloning $ES_REPO into $(pwd)..."
+    echo "Cloning $ES_REPO into $(pwd)..."
 
-	git clone --recurse-submodules https://github.com/$ES_REPO .
-	git checkout -f origin/$ES_BRANCH
+    git clone --recurse-submodules https://github.com/$ES_REPO .
+    git checkout -f origin/$ES_BRANCH
 
 # Setup
-	echo "Pre-run setup..."
-	cd $DIR
+    echo "Pre-run setup..."
+    cd $DIR
 
-	# SQL + Beatmap files
-		ln -s -f $DATA_DIR/sql $DIR/sql
-		ln -s -f $DATA_DIR/beatmaps $DIR/beatmaps
+    # SQL + Beatmap files
+        ln -s -f $DATA_DIR/sql $DIR/sql
+        ln -s -f $DATA_DIR/beatmaps $DIR/beatmaps
 
-	# Port
-		PORT_FILE=$DATA_DIR/port.dat
-		CURR_PORT=$(cat $PORT_FILE)
-		((CURR_PORT++))
-		echo $CURR_PORT > $PORT_FILE
+    # Port
+        PORT_FILE=$DATA_DIR/port.dat
+        CURR_PORT=$(cat $PORT_FILE)
+        ((CURR_PORT++))
+        echo $CURR_PORT > $PORT_FILE
 
-		echo "Setting up on port $CURR_PORT"
+        echo "Setting up on port $CURR_PORT"
 
-	# Nginx
-		NGINX_TEMPLATE=$DATA_DIR/nginx.tpl
-		SUBDOMAIN=${PWD##*/}
-		SITE_FILE=/etc/nginx/sites-enabled/$SUBDOMAIN
+    # Nginx
+        NGINX_TEMPLATE=$DATA_DIR/nginx.tpl
+        SUBDOMAIN=${PWD##*/}
+        SITE_FILE=/etc/nginx/sites-enabled/$SUBDOMAIN
 
-		echo "Using subdomain $SUBDOMAIN"
+        echo "Using subdomain $SUBDOMAIN"
 
-		cp $NGINX_TEMPLATE $SITE_FILE
-		sed -i "s/{DOMAIN}/$SUBDOMAIN/g" $SITE_FILE
-		sed -i "s/{PORT}/$CURR_PORT/g" $SITE_FILE
+        cp $NGINX_TEMPLATE $SITE_FILE
+        sed -i "s/{DOMAIN}/$SUBDOMAIN/g" $SITE_FILE
+        sed -i "s/{PORT}/$CURR_PORT/g" $SITE_FILE
 
-	# Environment
-		if $(grep -q 'ssl' $SITE_FILE); then
-			HTTP_SCHEME='https'
-		else
-			HTTP_SCHEME='http'
-		fi
+    # Environment
+        if $(grep -q 'ssl' $SITE_FILE); then
+            HTTP_SCHEME='https'
+        else
+            HTTP_SCHEME='http'
+        fi
 
-		HTTP_HOST=$(sed -n 's/^.*server_name\s*\(.*\);$/\1/p' $SITE_FILE)
+        HTTP_HOST=$(sed -n 's/^.*server_name\s*\(.*\);$/\1/p' $SITE_FILE)
 
 # Docker
-	export UID
-	export NGINX_PORT=$CURR_PORT
-	export HTTP_URL="$HTTP_SCHEME://$HTTP_HOST"
-	export MODE=$MODE
-	export MODE_LITERAL=$MODE_LITERAL
+    export UID
+    export NGINX_PORT=$CURR_PORT
+    export HTTP_URL="$HTTP_SCHEME://$HTTP_HOST"
+    export MODE=$MODE
+    export MODE_LITERAL=$MODE_LITERAL
 
-	./bin/docker_dev.sh
+    ./bin/docker_dev.sh
